cc2f1ee5413e07b2e52aea85533c46e206fa4ed0
Phillip <pnoreggio@gmail.com>
2017-01-31 21:32:41 -0500

    Small Commit + Moveset Component Class + Attack class

---
+++ core/src/com/mygdx/game/Attack.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+/**
+ * Created by pnore_000 on 1/31/2017.
+ */
+public interface Attack {
+    void effect();
+
+}
---
+++ core/src/com/mygdx/game/components/MovesetComponent.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by pnore_000 on 1/31/2017.
+ */
+public class MovesetComponent implements Component {
+}
